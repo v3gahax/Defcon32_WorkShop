8baa98704f910412545467de0f1f628997024b6e
Michael Aguilar <maguilar@secureworks.com>
2024-07-14 11:34:14 -0700

    Content Uploads

---
+++ ProtoCode/dll.c
@@ -0,0 +1,20 @@
+#include <Windows.h>
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+extern "C" __declspec(dllexport) void run()
+{
+	MessageBoxA(NULL, "Execution happened", "Bypass", MB_OK);
+}
